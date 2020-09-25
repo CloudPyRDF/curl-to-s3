bb905f453d490d69e977b5a0ad42f970ddf9ed8d
Piotr Pasternak <piotrpassternak@gmail.com>
2020-09-25 20:02:22 +0200

    change to connect to eos

--- lambda.py
+++ lambda.py
@@ -5,8 +5,12 @@ import urllib3
 
 def lambda_handler(event, context):
 
+    eos_url = 'https://cernbox.cern.ch/cernbox/webdav/eos/'
+
     http = urllib3.PoolManager()
-    r = http.request('GET', event['url'])
+    url = eos_url + event['path'] + event['resource'] + '?login=' + event['login'] + '&password=' + event['password']
+    r = http.request('GET', url)
+    
     s3 = boto3.resource('s3')
     obj = s3.Object(event['bucket_name'], event['object_key'])
     obj.put(Body=r.data)
