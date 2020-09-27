a3b1d25cbbd94edb2da3fc2208c620b8c7888c5c
Piotr Pasternak <piotrpassternak@gmail.com>
2020-09-27 16:26:10 +0200

    update authorization method

--- lambda.py
+++ lambda.py
@@ -8,8 +8,9 @@ def lambda_handler(event, context):
     eos_url = 'https://cernbox.cern.ch/cernbox/webdav/eos/'
 
     http = urllib3.PoolManager()
-    url = eos_url + event['path'] + event['resource'] + '?login=' + event['login'] + '&password=' + event['password']
-    r = http.request('GET', url)
+    url = eos_url + event['path'] + event['resource']
+    header = urllib3.make_headers(basic_auth=event['login']+':'+event['password'])
+    r = http.request('GET', url, headers=header)
     
     s3 = boto3.resource('s3')
     obj = s3.Object(event['bucket_name'], event['object_key'])
