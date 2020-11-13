d03879da40472f4000c7bc0ee536f979cdab94df
Jacek Kuśnierz <jaku@grapeup.com>
2020-11-13 01:47:49 +0100

    more debug

--- lambda.py
+++ lambda.py
@@ -4,16 +4,19 @@ import urllib3
 
 
 def lambda_handler(event, context):
+    print("Started")
 
     eos_url = 'https://cernbox.cern.ch/cernbox/webdav/eos/'
-
     http = urllib3.PoolManager()
     url = eos_url + event['path'] + event['resource']
     header = urllib3.make_headers(basic_auth=event['login']+':'+event['password'])
+    print("Starting request")
     r = http.request('GET', url, headers=header)
-    
+    print("Finished request, putting into s3")
+
     s3 = boto3.resource('s3')
     obj = s3.Object(event['bucket_name'], event['object_key'])
+    
     obj.put(Body=r.data)
     return {
         'statusCode': 200,
