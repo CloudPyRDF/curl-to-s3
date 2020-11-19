0f4a6bf2a0971402e6a1f453f3a09dd1489b8045
Jacek Kuśnierz <jaku@grapeup.com>
2020-11-20 00:04:08 +0100

    update props

--- lambda.py
+++ lambda.py
@@ -8,14 +8,14 @@ def lambda_handler(event, context):
 
     eos_url = 'https://cernbox.cern.ch/cernbox/webdav/eos/'
     http = urllib3.PoolManager()
-    url = eos_url + event['path'] + event['resource']
-    header = urllib3.make_headers(basic_auth=event['login']+':'+event['password'])
+    url = eos_url + event['eos_path'] + event['eos_filename']
+    header = urllib3.make_headers(basic_auth=event['eos_login']+':'+event['eos_password'])
     print("Starting request")
     r = http.request('GET', url, headers=header)
     print("Finished request, putting into s3")
 
     s3 = boto3.resource('s3')
-    obj = s3.Object(event['bucket_name'], event['object_key'])
+    obj = s3.Object(event['s3_bucket_name'], event['s3_object_key'])
     
     obj.put(Body=r.data)
     return {
