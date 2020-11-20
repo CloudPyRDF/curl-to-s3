a6958984fc0674d050d5b476092e79d39f7f6503
Jacek Kuśnierz <jaku@grapeup.com>
2020-11-20 21:02:24 +0100

    streaming solution for better times

--- lambda.py
+++ lambda.py
@@ -1,4 +1,5 @@
 import json
+
 import boto3
 import urllib3
 
@@ -9,15 +10,18 @@ def lambda_handler(event, context):
     eos_url = 'https://cernbox.cern.ch/cernbox/webdav/eos/'
     http = urllib3.PoolManager()
     url = eos_url + event['eos_path'] + event['eos_filename']
-    header = urllib3.make_headers(basic_auth=event['eos_login']+':'+event['eos_password'])
-    print("Starting request")
-    r = http.request('GET', url, headers=header)
-    print("Finished request, putting into s3")
+    header = urllib3.make_headers(basic_auth=event['eos_login'] + ':' + event['eos_password'])
 
+    print("Starting request")
+
+    # this should stream the response straight to s3 bucket
     s3 = boto3.resource('s3')
-    obj = s3.Object(event['s3_bucket_name'], event['s3_object_key'])
-    
-    obj.put(Body=r.data)
+    s3.upload_fileobj(
+        Fileobj=http.request('GET', url, headers=header, preload_content=False),
+        Bucket=event['s3_bucket_name'],
+        Key=event['s3_object_key']
+    )
+
     return {
         'statusCode': 200,
         'body': json.dumps('Uploaded!')
